8a17911c2826fb6c469a0a6fb85f13674207d943
Hubert Lefevre <hubelefevre@gmail.com>
2014-06-01 01:17:54 +0200

    Sacla: TD2: traktTimestamp

--- scala/td2/build.sbt
+++ scala/td2/build.sbt
@@ -1,11 +1,14 @@
 scalaVersion := "2.11.0"
 
 libraryDependencies ++= Seq(
-                "com.netflix.rxjava" % "rxjava-scala" % "0.18.3")
+    "com.netflix.rxjava" % "rxjava-scala" % "0.18.3",
+    "org.specs2" %% "specs2" % "2.3.12" % "test",
+    "org.scalatest" % "scalatest_2.11" % "2.1.6" % "test",
+    "io.spray" %%  "spray-json" % "1.2.6")
 
-resolvers += "releases"  at "http://oss.sonatype.org/content/repositories/releases"
+resolvers ++= Seq(
+    "releases"  at "http://oss.sonatype.org/content/repositories/releases",
+    "spray" at "http://repo.spray.io/")
 
 scalacOptions ++= Seq("-deprecation", "-feature")
 
-//"org.specs2" %% "specs2" % "2.3.12" % "test",
-//                "org.scalatest" % "scalatest_2.11" % "2.1.6" % "test",
--- scala/td2/src/main/scala/td2.scala
+++ scala/td2/src/main/scala/td2.scala
@@ -2,22 +2,58 @@ package fr.plnc.td2
 
 import scala.io._
 import rx.lang.scala._
+import spray.json._
+import DefaultJsonProtocol._
+
+object TraktAPIVal {
+  // User ID
+  val traktAPIKey: String = ??? // Enter here your API key
+
+  // API url requests
+  val traktAPIURL: String = "http://api.trakt.tv/"
+
+  // Time request
+  val traktAPIServerTime: String = traktAPIURL + "server/time.json/" + traktAPIKey
+}
 
 object TD2 {
-  def fromURL(url: String): Observable[Char] =
-    Observable.defer(Observable.from(scala.io.Source.fromURL(url).toIterable, schedulers.IOScheduler.apply))
+  import TraktAPIVal._
 
+  def fromURL(url: String): Observable[Char] =
+    Observable.defer(Observable.from(scala.io.Source.fromURL(url).toIterable,
+      schedulers.IOScheduler.apply))
+
+  // By the way, I don't know why this function must ise the fromURL function.
+  // The function scala.io.Source.fromURL returns a BufferedSource that dispose
+  // of a function to directly returns the content in String format.
   def getContent(url: String): Observable[String] =
-    Observable.defer( Observable[String] {
+    Observable.defer(Observable[String] {
     (subscriber: Subscriber[String]) =>
-      var observableFromURL: Observable[Char] = fromURL(url)
       var string: String = ""
 
-      observableFromURL.subscribe(
+      fromURL(url).subscribe(
         { (c: Char) => string = string :+ c },
         { (t: Throwable) => println(s"Exception: $t") },
         { () =>  subscriber.onNext(string)})
     })
+
+  def getJSON(url: String): Observable[JsValue] =
+    Observable.defer(Observable[JsValue] {
+      (subscriber: Subscriber[JsValue]) =>
+        getContent(url).subscribe(
+        { (c: String) => subscriber.onNext(c.parseJson)},
+        { (t: Throwable) => println(s"Exception: $t") },
+        { () => println("Observable is terminated") })
+    })
+
+  def traktTimestamp: Observable[Int] = (
+    Observable.defer(Observable[Int] {
+      (subscriber: Subscriber[Int]) =>
+        getJSON(traktAPIServerTime).subscribe(
+          { (j: JsValue) =>
+          subscriber.onNext(j.asJsObject.getFields("timestamp").head.convertTo[Int]) })
+      })
+  )
 }
 
 
@@ -25,19 +61,6 @@ object TD2 {
 object Main extends App
 {
   import TD2._
-
-  println("Start, instanciate fromURL(Google)")
-  //val google: Observable[Char] = fromURL("http://www.rfc1149.net/blog/")
-  //google.subscribe(
-  //  { n => println(n) },
-  //  { t: Throwable => println(s"Exception: $t") },
-  //  { () => println("Observable is terminated") })
-
-  val googleStr: Observable[String] = getContent("http://www.rfc1149.net/blog/")
-  googleStr.subscribe(
-    { n => println(n) },
-    { t: Throwable => println(s"Exception: $t") },
-    { () => println("Observable is terminated") })
-
+  traktTimestamp.subscribe({(t: Int) => println(s"Time: ${t}")})
 }
 
